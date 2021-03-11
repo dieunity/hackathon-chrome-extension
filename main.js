b63b7ed35352e7234fc56ba78dd89c5159710a83
Dieu Huynh <dieuhuynh@Dieus-MacBook-Pro.local>
2021-03-10 22:48:15 -0800

    implemented a Google Chrome extension to grayscale every website

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
 // console.log
 console.log('main.js is loaded');
-// document.getElementById("some element").style.filter = "grayscale(100%)";
+// document.getElementById("base_content").style.filter = "grayscale(100%)";
+// alter the style element of the root <html> node, setting filter to max grayscale
+document.documentElement.style.filter = "grayscale(100%)";
